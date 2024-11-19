2c4b2e68d0fb3ffef0b8014bcd4480ce58b27019
영진(Watson) <bryjna07@nate.com>
2024-11-19 09:46:47 +0900

    [level 0] Title: 문자열 섞기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- Swift/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
+++ Swift/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 func solution(_ str1:String, _ str2:String) -> String {
-    zip(str1, str2).map { String($0) + String($1) }.joined()
+    var result = ""
+    for i in 0..<str1.count {
+        let chr1 = str1[str1.index(str1.startIndex, offsetBy: i)]
+        let chr2 = str2[str2.index(str2.startIndex, offsetBy: i)]
+        result += String(chr1) + String(chr2)
+    }
+    return result
 }
